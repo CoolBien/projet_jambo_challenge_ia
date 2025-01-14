52750217f7bb78754405589fc6bf285bb3e60a18
CoolBien <elidrissi.sami89@gmail.com>
2025-01-14 17:36:20 +0100

    ComputePosition pour les cuts

--- Jumbo/src/jumbo/data/Cut.java
+++ Jumbo/src/jumbo/data/Cut.java
@@ -2,9 +2,17 @@ package jumbo.data;
 
 public record Cut(CutOrientation orientation, int sizeX, int sizeY, int itemFlipCoding, int... itemIds) {
 
-	public int computePosition() {
-		// TODO Auto-generated method stub
-		return 0;
+	public int computePosition(Instance instance) {
+		int positionCut = 0;
+		for (int i=0; i<itemIds.length; i++) {
+			if ((itemFlipCoding & (1 << i)) != 0) {
+				positionCut += instance.getItemHeigth(itemIds[i]);
+			}
+			else {
+				positionCut += instance.getItemWidth(itemIds[i]);
+			}
+		}
+		return positionCut;
 	}
 
 }
